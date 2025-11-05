53d9b42e1146a6bcf65863ed3fe5dd6fba56d805
Auguste124 <158301113+Vanakam20@users.noreply.github.com>
2025-11-05 10:53:27 +0100

    Update logout.php

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -1,7 +1,7 @@
 <?php
 // Supprime le cookie d'authentification
 setcookie('authToken', '', time() - 3600, '/', '', false, true); // Cookie expiré par la valeur -3600. Il n'est donc plus valable
-
+setcookie('authH', '', time() - 3600, '/', '', false, true);
 // Redirection vers la page de connexion
 header('Location: index.php');
 exit();
